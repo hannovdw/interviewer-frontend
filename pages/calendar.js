2a517f7f5250e12dae8bfd0c6dd42441fda9256d
hanno <hannovdw1998@gmail.com>
2024-01-02 14:11:11 +0200

    calender page

--- pages/calendar.js
+++ pages/calendar.js
@@ -1,37 +1,86 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
-
-const events = [
-  {
-    title: 'Phone Interview',
-    start: '2023-12-13T10:00:00',
-    end: '2023-12-13T12:00:00',
-    candidate: 'John Doe',
-  },
-  {
-    title: 'In-Person Interview',
-    start: '2023-12-14T12:00:00',
-    end: '2023-12-14T14:00:00',
-    candidate: 'Jane Smith',
-  },
-
-];
+import { useRouter } from 'next/router';
 
 const Calendar = () => {
+
+  const router = useRouter();
+
+  const [interviews, setInterviews] = useState([]);
+
+  const handleToggleWeeks = (eventInfo) => {
+
+    const startDate = eventInfo.view.activeStart;
+    const endtDate = eventInfo.view.activeEnd;
+
+    fetchInterviews(startDate, endtDate);
+
+  };
+
+  const handleEventClick = (info) => {
+
+    const interviewId = info.event.id;
+
+    if (interviewId) {
+      router.push(`/interviews/${interviewId}`);
+    }
+
+  };
+
+  const fetchInterviews = async (startDate, endDate) => {
+
+    try {
+
+      const token = localStorage.getItem("token");
+
+      const formattedStartDate = startDate.toISOString().split('T')[0];
+      const formattedEndDate = endDate.toISOString().split('T')[0];
+
+      const response = await fetch(
+        `http://localhost:8080/api/v1/interviews/search/by-date?startDate=${formattedStartDate}&endDate=${formattedEndDate}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+
+        const data = await response.json();
+
+        const mappedInterviews = data.map((interview) => ({
+          id: interview.id,
+          title: `${interview.candidate.firstName} ${interview.candidate.lastName} - ${interview.title.titleName}`,
+          start: interview.dateTime,
+          end: interview.endDateTime,
+        }));
+
+        setInterviews(mappedInterviews);
+
+      } else {
+        console.error('Failed to fetch interviews:', response.status, response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching interviews:', error);
+    }
+
+  };
+
+
   return (
     <div>
       <FullCalendar
         plugins={[dayGridPlugin, timeGridPlugin]}
         initialView="timeGridWeek"
-        events={events}
-        eventClick={(info) => {
-          alert(
-            `Interview Type: ${info.event.title}\nCandidate: ${info.event.extendedProps.candidate}\nStart: ${info.event.start.toLocaleString()}\nEnd: ${info.event.end.toLocaleString()}`
-          );
-        }}
-        height="70vh"
+        events={interviews}
+        eventClick={handleEventClick}
+        height="85vh"
+        datesSet={handleToggleWeeks}
       />
     </div>
   );
